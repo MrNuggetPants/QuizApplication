f97d80ea893535f464d718f658292a481cf795a7
Braden <bradenkayne@gmail.com>
2023-04-07 16:44:45 +1000

    yeet

---
+++ app/src/main/java/com/example/quizapp/Questions.java
@@ -0,0 +1,22 @@
+package com.example.quizapp;
+
+public class Questions {
+
+    public static String question[] = {
+            "Who is this?",
+            "What anime is this from?",
+            "Are you gay?"
+    };
+
+    public static String choices[][] = {
+            {"Kakashi", "Gojo", "Eren", "Chicken"},
+            {"JJK", "AOT", "JOJO", "SAO"},
+            {"Yes", "Yeah!", "Yip", "Yas"}
+    };
+
+    public static String correctAnswers[] = {
+            "Gojo",
+            "AOT",
+            "Yes"
+    };
+}
--- app/src/main/java/com/example/quizapp/QuizActivity.java
+++ app/src/main/java/com/example/quizapp/QuizActivity.java
@@ -1,4 +1,19 @@
 package com.example.quizapp;
 
-public class QuizActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuizActivity extends AppCompatActivity {
+    TextView totalQuestionsTextView;
+    TextView questionTextView;
+    Button ansA, ansB, ansC, ansD;
+    Button submit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz_main);
+    }
 }
